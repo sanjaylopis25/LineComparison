58710dc660b1ba4df1001619eb26a125cba729ad
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 16:02:58 +0530

    modified for the UC3 program

--- Program.cs
+++ Program.cs
@@ -35,14 +35,16 @@ namespace LineComprison
             double PowofpointsX = Math.Pow(2, (X2 - X1));
             double PowofpointsY = Math.Pow(2, (Y2 - Y1));
             double LengthofLine1 = Math.Sqrt((PowofpointsX + PowofpointsY));
-            Console.WriteLine("Length of the Line is: " + LengthofLine1);
+            Console.WriteLine("Length of the First Line is: " + LengthofLine1);
 
             double PowofpointsA = Math.Pow(2, (A2 - A1));
             double PowofpointsB = Math.Pow(2, (B2 - B1));
             double LengthofLine2 = Math.Sqrt((PowofpointsA + PowofpointsB));
-            Console.WriteLine("Length of the Line is: " + LengthofLine2);
+            Console.WriteLine("Length of the Second Line is: " + LengthofLine2);
 
-            Console.WriteLine(LengthofLine1.Equals(LengthofLine2));
+            Console.WriteLine(LengthofLine1.CompareTo(LengthofLine2));
+            //Above syntax compares the length of line 1 with length of line 2. Here if length of line 1 is bigger it 
+            //gives the ouput as 1, if equals then 0 and if smaller gives the ouput as -1.
             Console.ReadLine();
         }
     }
