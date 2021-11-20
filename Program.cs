9161a3e1d81d3a4d61dbae8a3a104852c05302ac
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 10:38:14 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComprison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Program");
+            Console.ReadLine();
+        }
+    }
+}
