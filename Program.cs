6bd02f1ca83cfc78355a842f60b1bf1dfedab092
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 11:49:26 +0530

    modified for the UC1 program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace LineComprison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparison Program");
+            Console.Write("Enter the Co-rdinate X1: ");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate Y1: ");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate X2: ");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate Y2: ");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Powofpoints1 = Math.Pow(2, (X2 - X1));
+            double Powofpoints2 = Math.Pow(2, (Y2 - Y1));
+            double LengthofLine = Math.Sqrt((Powofpoints1 + Powofpoints2));
+            Console.WriteLine("Length of the Line is: " + LengthofLine);
             Console.ReadLine();
         }
     }
