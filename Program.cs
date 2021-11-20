5df4ad24ae21eda296d0e570320ca86e616e3d45
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-20 15:20:44 +0530

    modified for the UC2 program

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace LineComprison
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("First Line");
             Console.Write("Enter the Co-rdinate X1: ");
             int X1 = Convert.ToInt32(Console.ReadLine());
 
@@ -18,10 +19,30 @@ namespace LineComprison
             Console.Write("Enter the Co-rdinate Y2: ");
             int Y2 = Convert.ToInt32(Console.ReadLine());
 
-            double Powofpoints1 = Math.Pow(2, (X2 - X1));
-            double Powofpoints2 = Math.Pow(2, (Y2 - Y1));
-            double LengthofLine = Math.Sqrt((Powofpoints1 + Powofpoints2));
-            Console.WriteLine("Length of the Line is: " + LengthofLine);
+            Console.WriteLine("Second Line");
+            Console.Write("Enter the Co-rdinate A1: ");
+            int A1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate B1: ");
+            int B1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate A2: ");
+            int A2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate B2: ");
+            int B2 = Convert.ToInt32(Console.ReadLine());
+
+            double PowofpointsX = Math.Pow(2, (X2 - X1));
+            double PowofpointsY = Math.Pow(2, (Y2 - Y1));
+            double LengthofLine1 = Math.Sqrt((PowofpointsX + PowofpointsY));
+            Console.WriteLine("Length of the Line is: " + LengthofLine1);
+
+            double PowofpointsA = Math.Pow(2, (A2 - A1));
+            double PowofpointsB = Math.Pow(2, (B2 - B1));
+            double LengthofLine2 = Math.Sqrt((PowofpointsA + PowofpointsB));
+            Console.WriteLine("Length of the Line is: " + LengthofLine2);
+
+            Console.WriteLine(LengthofLine1.Equals(LengthofLine2));
             Console.ReadLine();
         }
     }
